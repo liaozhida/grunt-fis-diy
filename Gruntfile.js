300ed46e14ed7e6220c8797019ba86041a4735dc
xiejun <xiejun@umiit.cn>
2016-03-31 03:20:05 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib: {
+				src: ['lib/**/*.js']
+			},
+			tasks: {
+				src: ['tasks/**/*.js']
+			},
+			bin: {
+				src: ['bin/**/*.js']
+			},
+		},
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Default task.
+	grunt.registerTask('default', ['jshint']);
+
+};
